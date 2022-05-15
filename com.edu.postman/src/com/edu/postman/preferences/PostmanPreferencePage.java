90d97f6bfe014ab462ef05dc7f908dadcaf180d9
Eduardo SantAna <eduardosantana1313@gmail.com>
2022-05-15 12:13:31 -0300

    master	change postman preferences.

--- com.edu.postman/src/com/edu/postman/preferences/PostmanPreferencePage.java
+++ com.edu.postman/src/com/edu/postman/preferences/PostmanPreferencePage.java
@@ -1,7 +1,13 @@
 package com.edu.postman.preferences;
 
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
-import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
@@ -14,34 +20,48 @@ import com.edu.postman.service.postman.PostmanRepository;
 public class PostmanPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
     /**
-     * StringFieldEditor - PostmanPreferencePage.java
+     * Text - PostmanPreferencePage.java
      */
-    private StringFieldEditor editor;
+    private Text textApiKey;
 
     /**
      * New instance to PostmanPreferencePage.
      */
     public PostmanPreferencePage() {
         super(GRID);
+    }
+
+    @Override
+    public void init(final IWorkbench arg0) {
         setDescription(Messages.PostmanMessage_PostmanPreferences);
+    }
+
+    @Override
+    protected Control createContents(final Composite parent) {
+
+        final Composite container = new Composite(parent, SWT.NONE);
+        container.setLayout(new GridLayout(2, false));
+
+        new Label(container, SWT.NONE).setText("Api Key");
+
+        textApiKey = new Text(container, SWT.BORDER);
+        textApiKey.setToolTipText("PMAK-1234aabb12abcd1a1a1abcd1-a1234a12ab12a1234a1a1a12ab12345678");
+        textApiKey.setMessage("PMAK-1234aabb12abcd1a1a1abcd1-a1234a12ab12a1234a1a1a12ab12345678");
+        textApiKey.setText(PostmanRepository.getInstance().getApiKey());
+        textApiKey.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+
+        return super.createContents(parent);
     }
 
     @Override
     public boolean performOk() {
 
-        return PostmanRepository.getInstance().save(editor.getStringValue());
+        return PostmanRepository.getInstance().save(textApiKey.getText());
     }
 
     @Override
     public void createFieldEditors() {
-        editor = new StringFieldEditor("API_KEY", "API Key", getFieldEditorParent());
-        editor.setStringValue(PostmanRepository.getInstance().getApiKey());
-        addField(editor);
-    }
-
-    @Override
-    public void init(final IWorkbench workbench) {
-
+        // NA
     }
 
 }
